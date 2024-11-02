a3809f6ceb779a75e985fc83431c3f9f38828379
hayashi-yaken <mypodo@icloud.com>
2024-11-02 18:06:11 +0900

    テスト用の修正

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,9 @@
+import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  plugins: [react()],
   resolve: {
     alias: {
       '@': '/src',
@@ -9,7 +12,14 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    coverage: { include: ['src/**'] },
+    coverage: {
+      include: ['src/components/**'],
+      exclude: [
+        'src/components/**/index.ts',
+        'src/components/**/story',
+        'src/components/**/test',
+      ],
+    },
     setupFiles: ['./setupTests.ts'],
   },
 })
