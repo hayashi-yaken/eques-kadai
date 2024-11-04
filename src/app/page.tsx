0c8f8e6daacad28f74a5746387aea2f5461c28a0
hayashi-yaken <mypodo@icloud.com>
2024-11-04 09:52:54 +0900

    設定ページを作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,29 @@
-export default function Home() {
-  return <></>
+'use client'
+
+import { Box } from '@mui/material'
+import React from 'react'
+
+import { SideBar } from '@/components/SideBar'
+import { SideDetail } from '@/components/SideDetail'
+
+import { Setting } from '@/features/setting'
+
+export default function Component() {
+  const sideBarWidth = 70
+  const sideDetailWidth = 200
+  return (
+    <Box display='flex'>
+      <Box display='flex'>
+        <SideBar width={sideBarWidth} />
+        <SideDetail
+          title='設定'
+          categories={['ユーザー設定']}
+          properties={['アカウント情報', 'パスワード変更', '2段階認証']}
+          sideBarWidth={sideBarWidth}
+          sideDetailWidth={sideDetailWidth}
+        />
+      </Box>
+      <Setting sideBarWidth={sideBarWidth} sideDetailWidth={sideDetailWidth} />
+    </Box>
+  )
 }
